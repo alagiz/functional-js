bc579a6e2ef125c588c9ba2680dfb1bc65b156a3
ArtemAlagizov <alagizov@gmail.com>
2018-07-08 17:36:43 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const log = require('./lib/log')
+const r = require('ramda')
+
+const band = {
+  name: 'da best band',
+  members: {
+    current: [{
+      name: 'wee biscuit mcgee',
+      plays: ['harmonica']
+    }, {
+      name: 'johnny deep',
+      plays: ['harmonica', 'guitar', 'hang drum']
+    }, {
+      name: 'robocop',
+      plays: ['instrument of murder', 'beeper']
+    }],
+    past: [{
+      name: 'pastor gains',
+      plays: ['bench', 'dumbbels']
+    }, {
+      name: 'foxy brown',
+      plays: ['pianod', 'harp']
+    }]
+  }
+}
+
+const makeUpper = a => a.toUpperCase();
+const name = r.lensProp('name');
+const lowerName = r.compose(makeUpper, r.prop('name'))
+const currentMembers = r.lensPath(['members', 'current'])
+const previousMembers = r.lensPath(['members', 'past'])
+const upperCurrentMemberNames = r.over(currentMembers, r.map(r.over(name, makeUpper)))
+const omitPreviousMembersPlays = r.over(previousMembers, r.map(r.omit('plays')))
+
+const mods = [
+	upperCurrentMemberNames,
+	omitPreviousMembersPlays
+]
+
+log('--------------------------------------------------------')
+log(r.compose(...mods)(band))
